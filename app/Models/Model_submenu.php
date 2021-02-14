2645ce0daa747c3a9dac775a349be5e3b61fb3dc
Nemure231 <neruhi5321@gmail.com>
2021-02-14 22:59:39 +0700

    Menambahkan model submenu

---
+++ app/Models/Model_submenu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Model_submenu extends Model
+{
+    protected $table = 'submenu';
+    protected $primaryKey = 'id_submenu';
+    protected $fillable = ['nama_submenu', 'menu_id', 'route_submenu', 'url_submenu'];
+}
