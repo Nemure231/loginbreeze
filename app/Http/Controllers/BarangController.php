2126d050fdcf5f9344d1d4a1e40c99d6da227f3f
Nemure231 <neruhi5321@gmail.com>
2021-12-24 16:55:11 +0700

    nyoba dd relasi akses menu ganti lagi

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -37,7 +37,7 @@ class BarangController extends Controller
         ->orderBy('akses_menu.menu_id', 'asc')
         ->orderBy('akses_menu.role_id', 'asc')
         ->get();
-        dd($td[0]['menu']['nama_menu']);
+        dd($td);
 
 
         $role = Auth::user()->role_id;
